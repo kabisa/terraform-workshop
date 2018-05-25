14dff6be7635c78de6456e172a3407992cf0cb98
Luk van den Borne <luk.van.den.borne@kabisa.nl>
2018-05-25 14:50:38 +0200

    Code after assignment 2.

--- ec2.tf
+++ ec2.tf
@@ -9,16 +9,6 @@ resource "aws_instance" "kabisa-demo-instance" {
     volume_type = "gp2"
     volume_size = 8
   }
-
-  user_data = <<EOF
-#!/bin/sh
-apt-get update
-apt-get install -y apache2 libapache2-mod-php php-mysql
-wget https://wordpress.org/latest.tar.gz
-tar xpzf latest.tar.gz --strip 1 -C /var/www/html/ wordpress
-rm /var/www/html/index.html
-chown -R www-data:www-data /var/www/html/
-EOF
 }
 
 /* Elastic ip */
